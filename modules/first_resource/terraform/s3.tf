93146c6e3a545e985c55e1acd00e97076f4ebc94
visweswarji <visweswar5555955@gmail.com>
2024-05-30 16:32:04 +0530

    added provider

--- modules/first_resource/terraform/s3.tf
+++ modules/first_resource/terraform/s3.tf
@@ -7,3 +7,8 @@ resource "aws_s3_bucket" "s3buckrocks" {
   bucket = "s3buckrocks"
   acl    = "private"
 }
+provider "aws" {
+  region = "us-east-1"
+  # Add any other required provider configurations here
+  #test
+}
