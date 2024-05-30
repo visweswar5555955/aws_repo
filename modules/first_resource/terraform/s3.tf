ad3a96139141817a9c18d6909457f1e91b93aae8
visweswarji <visweswar5555955@gmail.com>
2024-05-30 17:36:08 +0530

    pusehed

--- modules/first_resource/terraform/s3.tf
+++ modules/first_resource/terraform/s3.tf
@@ -1,11 +1,24 @@
-
-resource "aws_s3_bucket" "mumbai_bucket" {
-  bucket = "mumbai-bucket"
-
-  # Other required arguments...
+locals {
+   bucket_name = "noname-production-default-storage"
 }
-
-resource "aws_s3_bucket_acl" "mumbai_bucket_acl" {
-  bucket = aws_s3_bucket.mumbai_bucket.id
-  //acl    = "private"
+    
+resource "aws_s3_bucket" "noname-production-s3" {
+  bucket = local.bucket_name
 }
+    
+# resource "aws_s3_bucket_policy" "noname-s3-policy" {
+#   bucket = aws_s3_bucket.noname-production-s3.id
+    
+#   policy = jsonencode({
+#     Version = "2012-10-17",
+#     Statement = [
+#        {
+#           Sid = "PublicReadGetObject",
+#           Effect = "Allow",
+#           Principal = "*",
+#           Action = "s3:GetObject",
+#           Resource = "arn:aws:s3:::${local.bucket_name}/*"
+#        }
+#      ]
+#   })
+# }
